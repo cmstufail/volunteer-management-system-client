2252c4255ada8129ac2ad007b46b94158c4675f6
cmstufail <cmstufail@gmail.com>
2025-08-11 10:46:41 +0600

    updated use axios secure hook

--- src/pages/shared/hooks/useAxiosSecure.js
+++ src/pages/shared/hooks/useAxiosSecure.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect } from 'react';
 import { useAuth } from '../../../context/AuthProvider';
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 const axiosSecure = axios.create( {
     baseURL: import.meta.env.VITE_API_URL,
@@ -13,7 +14,26 @@ const useAxiosSecure = () => {
     const navigate = useNavigate();
 
     useEffect( () => {
-        const interceptor = axiosSecure.interceptors.response.use(
+        const requestInterceptor = axiosSecure.interceptors.request.use(
+            ( config ) => {
+                const token = Cookies.get( 'token' );
+                if ( token ) {
+                    config.headers.Authorization = `Bearer ${ token }`;
+                }
+                return config;
+            },
+            ( error ) => {
+                return Promise.reject( error );
+            }
+        );
+
+        return () => {
+            axiosSecure.interceptors.request.eject( requestInterceptor );
+        };
+    }, [] );
+
+    useEffect( () => {
+        const responseInterceptor = axiosSecure.interceptors.response.use(
             response => response,
             async ( error ) => {
                 if ( error.response && ( error.response.status === 401 || error.response.status === 403 ) ) {
@@ -25,7 +45,7 @@ const useAxiosSecure = () => {
         );
 
         return () => {
-            axiosSecure.interceptors.response.eject( interceptor );
+            axiosSecure.interceptors.response.eject( responseInterceptor );
         };
 
     }, [ logout, navigate ] );
